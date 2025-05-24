013b33b1ff84119101403432c6cbe4677aa3f8d8
William <wgrantbowley@gmail.com>
2025-05-24 18:21:17 +1000

    Design code is a mess
    Added:
    - Articles & Hover effect
    - Wrapper for all content. So its inline.
    - More Color Palettes

--- code/static/javascript.js
+++ code/static/javascript.js
@@ -59,3 +59,49 @@ boxes.forEach(box => {
   });
 });
 
+
+const articleboxes = document.querySelectorAll('.article'); // NOT .project-box
+
+articleboxes.forEach(box => {
+  let currentX = 50;
+  let currentY = 50;
+  let targetX = 50;
+  let targetY = 50;
+
+  const ease = 0.15;
+
+  function animate() {
+    currentX += (targetX - currentX) * ease;
+    currentY += (targetY - currentY) * ease;
+
+    box.style.setProperty('--x', `${currentX}%`);
+    box.style.setProperty('--y', `${currentY}%`);
+
+    requestAnimationFrame(animate);
+  }
+
+  animate();
+
+  box.addEventListener('mousemove', e => {
+    const rect = box.getBoundingClientRect();
+    targetX = ((e.clientX - rect.left) / rect.width) * 100;
+    targetY = ((e.clientY - rect.top) / rect.height) * 100;
+  });
+
+  box.addEventListener('mouseleave', () => {
+    targetX = 50;
+    targetY = 50;
+  });
+});
+
+
+document.querySelectorAll('.article[data-href]').forEach(article => {
+  article.style.cursor = 'pointer'; // make cursor pointer
+
+  article.addEventListener('click', () => {
+    const url = article.getAttribute('data-href');
+    if (url) {
+      window.location.href = url;
+    }
+  });
+});
